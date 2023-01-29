0418105096af2cdc1c53d7ceb3b6baaf4bab4f86
deerpearl <deerpearl@gmail.com>
2023-01-29 11:33:51 -0500

    add simple Express server

---
+++ server/src/index.ts
@@ -0,0 +1,37 @@
+import express, { Application } from 'express';
+import { ApolloServer, Config, gql } from 'apollo-server-express';
+import { IResolvers } from '@graphql-tools/utils';
+
+const typeDefs = gql`
+    type Query {
+        message: String!
+    }
+`
+const resolvers: IResolvers = {
+    Query: {
+        message: () => 'it works!'
+    }
+}
+
+const config : Config = {
+    typeDefs: typeDefs,
+    resolvers: resolvers
+} 
+
+
+async function startApolloServer(config: Config) {
+    const PORT = 8080;
+    const app: Application = express();
+    const server:ApolloServer = new ApolloServer(config);
+    await server.start();
+    server.applyMiddleware({
+        app,
+        path: './graphql'
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Server is running at http://localhost:${PORT}`);
+    });
+}
+
+startApolloServer(config);
